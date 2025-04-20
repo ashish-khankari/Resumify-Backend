9a88ff0914b3fd67e4371f453da6d1f4da766cfb
Ashish Khankari <ashishkhankari0927@gmail.com>
2025-04-20 09:17:30 +0530

    defined User Shema

--- models/user.ts
+++ models/user.ts
@@ -4,5 +4,24 @@ import { MONGO_URI } from "../config/envConfig";
 mongoose.connect(MONGO_URI);
 
 const userSchema = new mongoose.Schema({
-    
-})
+    username: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        unique: true,
+        reuired: true
+    },
+    password: {
+        tyoe: String,
+        required: true
+    },
+    workExperience: {
+        type: String,
+        reuired: true,
+    },
+});
+
+const userModel = mongoose.model("User", userSchema);
+export default userModel;
